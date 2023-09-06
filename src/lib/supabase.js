e9f1fbb195c9b19d26fe6852f9d79f45b29e7186
xYooki <yukitheegg@gmail.com>
2023-09-06 15:07:14 +1000

    mocking up a login verification

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -13,22 +13,8 @@ const { data1, error1 } = await supabase
   `)
 
   //Third party Oauth with Github(COMMENTED OUT DUE TO VISCODE NOT LIKING BOTH)
-  let { data, error } = await supabase.auth.signInWithOAuth({
+  /* let { data, error } = await supabase.auth.signInWithOAuth({
   provider: 'github'
-  })
+  }) */
 
-  // Example using axios, but you can use any HTTP library
-// const axios = require('axios');
-
-// const code = 'OAuth code received from GitHub';
-// const clientId = '43bd4b232664c5ca0764';
-// const clientSecret = 'ed3d055349efc91cb9565dd00ed3af7a6bebe9e4';
-
-// const response = await axios.post('https://github.com/login/oauth/access_token', {
-//   code,
-//   client_id: clientId,
-//   client_secret: clientSecret,
-// });
-// const accessToken = response.data.access_token;
-
- 
+  
